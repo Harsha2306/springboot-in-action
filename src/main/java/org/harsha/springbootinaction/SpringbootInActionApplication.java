81109a584b108d0a92b42c03e795493827f18338
harsharevanthmanchikanti <hr15171517hr@gmail.com>
2025-08-28 19:02:14 +0530

    initialization

---
+++ src/main/java/org/harsha/springbootinaction/SpringbootInActionApplication.java
@@ -0,0 +1,11 @@
+package org.harsha.springbootinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInActionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInActionApplication.class, args);
+    }
+}
